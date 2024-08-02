e6e0f8c91a4db7ac2ce8eac3cc6df6ba7db0fc9e
Gor Ter-Balyants <gorterbalyants@gmail.com>
2024-08-03 02:53:37 +0400

    .

--- Prediction/prediction.py
+++ Prediction/prediction.py
@@ -12,7 +12,16 @@ def preprocess_image(img, target_size=(224, 224)):
     return img_array
 
 
-def predict_breed(model, img_array):
+def predict_breed(model, img_array, version):
+    global class_names
+    match version:
+        case 1:
+            class_names = class_names1
+        case 2:
+            class_names = class_names2
+        case 3:
+            class_names = class_names3
+
     # Predict the class probabilities
     predictions = model.predict(img_array)
     # Get the predicted class index
@@ -22,31 +31,39 @@ def predict_breed(model, img_array):
     return predicted_breed
 
 
-class_names = ['Affenpinscher', 'Afghan Hound', 'Airedale Terrier', 'Akita', 'Alaskan Malamute', 'American Bulldog',
-               'American Cocker Spaniel', 'American Eskimo Dog', 'American Foxhound', 'American Hairless Terrier',
-               'American Pit Bull Terrier', 'American Staffordshire Terrier', 'American Water Spaniel',
-               'Anatolian Shepherd Dog', 'Australian Cattle Dog', 'Australian Shepherd', 'Australian Terrier',
-               'Azawakh', 'Basenji', 'Basset Hound', 'Beagle', 'Bearded Collie', 'Bloodhound', 'Border Terrier',
-               'Borzoi', 'Boston Terrier', 'Bouvier des Flandres', 'Boxer', 'Boykin Spaniel', 'Briard', 'Brittany dog',
-               'Brussels Griffon', 'Bull Terrier', 'Bulldog', 'Bullmastiff', 'Cairn Terrier', 'Canaan Dog',
-               'Cane Corso', 'Cardigan Welsh Corgi', 'Cavalier King Charles Spaniel', 'Cesky Terrier',
-               'Chesapeake Bay Retriever', 'Chihuahua', 'Chinese Crested', 'Chinese Shar-Pei', 'Chinook Dog',
-               'Chow Chow', 'Clumber Spaniel', 'Cockapoo', 'Cocker Spaniel', 'Collie', 'Coonhound', 'Corgi',
-               'Coton de Tulear', 'Curly-Coated Retriever', 'Dachshund', 'Dalmatian', 'Dandie Dinmont Terrier',
-               'Doberman Pinscher', 'Dogue de Bordeaux', 'Dutch Shepherd', 'English Bulldog', 'English Cocker Spaniel',
-               'English Foxhound', 'English Setter', 'English Springer Spaniel', 'English Toy Spaniel',
-               'Entlebucher Mountain Dog', 'Eskimo Dog', 'Field Spaniel', 'Finnish Lapphund', 'Finnish Spitz',
-               'Flat-Coated Retriever', 'French Bulldog', 'German Pinscher', 'German Shepherd',
-               'German Shorthaired Pointer', 'German Wirehaired Pointer', 'Giant Schnauzer', 'Glen of Imaal Terrier',
-               'Golden Retriever', 'Goldendoodle', 'Gordon Setter', 'Great Dane', 'Great Pyrenees',
-               'Greater Swiss Mountain Dog', 'Greyhound', 'Harrier Dog', 'Havanese', 'Ibizan Hound',
-               'Icelandic Sheepdog', 'Irish Red and White Setter', 'Irish Setter', 'Irish Terrier',
-               'Irish Water Spaniel', 'Irish Wolfhound', 'Italian Greyhound', 'Jack Russell Terrier',
-               'Japanese Chin', 'Japanese Spitz', 'Keeshond', 'Kerry Blue Terrier', 'Komondor', 'Kuvasz', 'Labradoodle',
-               'Labrador Retriever', 'Lakeland Terrier', 'Leonberger', 'Lhasa Apso', 'Lowchen', 'Maltese',
-               'Manchester Terrier', 'Mastiff', 'Miniature Bull Terrier', 'Miniature Pinscher', 'Miniature Schnauzer',
-               'Mudi', 'Neapolitan Mastiff', 'Newfoundland', 'Norfolk Terrier', 'Norwegian Buhund',
-               'Norwegian Elkhound', 'Norwegian Lundehund', 'Norwich Terrier', 'Nova Scotia Duck Tolling Retriever',
-               'Old English Sheepdog', 'Otterhound', 'Papillon', 'Pekingese', 'Pembroke Welsh Corgi',
-               'Petit Basset Griffon Vendeen', 'Pharaoh Hound', 'Plott', 'Pointer', 'Polish Lowland Sheepdog',
-               'Pomeranian', 'Poodle', 'Portuguese Water Dog', 'Pug', 'Puli', 'Pumi', 'Siberian Husky']
+class_names1 = ['Afghan Hound', 'Doberman Pinscher']
+
+class_names2 = ['Beagle', 'Bulldog', 'Dachshund', 'Doberman Pinscher', 'French Bulldog', 'German Shepherd',
+                'Golden Retriever', 'Labrador Retriever', 'Poodle', 'Siberian Husky']
+
+class_names3 = ['Affenpinscher', 'Afghan Hound', 'Airedale Terrier', 'Akita', 'Alaskan Malamute', 'American Bulldog',
+                'American Cocker Spaniel', 'American Eskimo Dog', 'American Foxhound', 'American Hairless Terrier',
+                'American Pit Bull Terrier', 'American Staffordshire Terrier', 'American Water Spaniel',
+                'Anatolian Shepherd Dog', 'Australian Cattle Dog', 'Australian Shepherd', 'Australian Terrier',
+                'Azawakh', 'Basenji', 'Basset Hound', 'Beagle', 'Bearded Collie', 'Bloodhound', 'Border Terrier',
+                'Borzoi', 'Boston Terrier', 'Bouvier des Flandres', 'Boxer', 'Boykin Spaniel', 'Briard', 'Brittany dog',
+                'Brussels Griffon', 'Bull Terrier', 'Bulldog', 'Bullmastiff', 'Cairn Terrier', 'Canaan Dog',
+                'Cane Corso', 'Cardigan Welsh Corgi', 'Cavalier King Charles Spaniel', 'Cesky Terrier',
+                'Chesapeake Bay Retriever', 'Chihuahua', 'Chinese Crested', 'Chinese Shar-Pei', 'Chinook Dog',
+                'Chow Chow', 'Clumber Spaniel', 'Cockapoo', 'Cocker Spaniel', 'Collie', 'Coonhound', 'Corgi',
+                'Coton de Tulear', 'Curly-Coated Retriever', 'Dachshund', 'Dalmatian', 'Dandie Dinmont Terrier',
+                'Doberman Pinscher', 'Dogue de Bordeaux', 'Dutch Shepherd', 'English Bulldog', 'English Cocker Spaniel',
+                'English Foxhound', 'English Setter', 'English Springer Spaniel', 'English Toy Spaniel',
+                'Entlebucher Mountain Dog', 'Eskimo Dog', 'Field Spaniel', 'Finnish Lapphund', 'Finnish Spitz',
+                'Flat-Coated Retriever', 'French Bulldog', 'German Pinscher', 'German Shepherd',
+                'German Shorthaired Pointer', 'German Wirehaired Pointer', 'Giant Schnauzer', 'Glen of Imaal Terrier',
+                'Golden Retriever', 'Goldendoodle', 'Gordon Setter', 'Great Dane', 'Great Pyrenees',
+                'Greater Swiss Mountain Dog', 'Greyhound', 'Harrier Dog', 'Havanese', 'Ibizan Hound',
+                'Icelandic Sheepdog', 'Irish Red and White Setter', 'Irish Setter', 'Irish Terrier',
+                'Irish Water Spaniel', 'Irish Wolfhound', 'Italian Greyhound', 'Jack Russell Terrier',
+                'Japanese Chin', 'Japanese Spitz', 'Keeshond', 'Kerry Blue Terrier', 'Komondor', 'Kuvasz',
+                'Labradoodle',
+                'Labrador Retriever', 'Lakeland Terrier', 'Leonberger', 'Lhasa Apso', 'Lowchen', 'Maltese',
+                'Manchester Terrier', 'Mastiff', 'Miniature Bull Terrier', 'Miniature Pinscher', 'Miniature Schnauzer',
+                'Mudi', 'Neapolitan Mastiff', 'Newfoundland', 'Norfolk Terrier', 'Norwegian Buhund',
+                'Norwegian Elkhound', 'Norwegian Lundehund', 'Norwich Terrier', 'Nova Scotia Duck Tolling Retriever',
+                'Old English Sheepdog', 'Otterhound', 'Papillon', 'Pekingese', 'Pembroke Welsh Corgi',
+                'Petit Basset Griffon Vendeen', 'Pharaoh Hound', 'Plott', 'Pointer', 'Polish Lowland Sheepdog',
+                'Pomeranian', 'Poodle', 'Portuguese Water Dog', 'Pug', 'Puli', 'Pumi', 'Siberian Husky']
+
+class_names = class_names1
--- main.py
+++ main.py
@@ -7,25 +7,96 @@ from io import BytesIO
 from PIL import Image
 from dotenv import load_dotenv
 import os
+import datetime
 
 load_dotenv()  # Load environment variables from .env file
 TOKEN = os.getenv('USER_TOKEN')
 BOT_USERNAME: Final = '@doggy_breeds_bot'
 
-model = tf.keras.models.load_model('Prediction/dog_breed_classifier.keras')
+
+model1 = tf.keras.models.load_model('Prediction/dog_breed_classifier_2.keras')
+model2 = tf.keras.models.load_model('Prediction/dog_breed_classifier_10.keras')
+model3 = tf.keras.models.load_model('Prediction/dog_breed_classifier.keras')
+
+user_sessions = {}
+
+
+async def create_user_session(user_id):
+    user_sessions[user_id] = {
+        'current_state': 'waiting_for_input',
+        'previous_inputs': [],
+        'model': model1,
+        'version': 1
+        }
+    print(f'{len(user_sessions)} Users')
+    print(user_sessions)
+
+
+async def update_user_session(user_id, key, value):
+    if user_id in user_sessions:
+        user_sessions[user_id][key] = value
+
+
+async def get_user_session(user_id):
+    return user_sessions.get(user_id, None)
 
 
 # Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello")
+    user_id = update.effective_user.id
+    if user_id not in user_sessions:
+        await create_user_session(user_id)
+    await update.message.reply_text("Hi, Select the model and start predicting!")
+
+
+async def restart_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+
+    await update_user_session(user_id, 'model', model1)
+    await update_user_session(user_id, 'version', 1)
+
+    await update.message.reply_text("Restarting...")
+
+
+async def first_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+
+    print(f'{user_id} User picked model 1', datetime.datetime.now())
+
+    await update_user_session(user_id, 'model', model1, )
+    await update_user_session(user_id, 'version', 1)
+
+    await update.message.reply_text("Model 1 Selected!")
+
+
+async def second_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+
+    print(f'{user_id} User picked model 2', datetime.datetime.now())
+
+    await update_user_session(user_id, 'model', model2)
+    await update_user_session(user_id, 'version', 2)
+
+    await update.message.reply_text("Model 2 Selected!")
+
+
+async def final_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+
+    print(f'{user_id} User picked final model', datetime.datetime.now())
+
+    await update_user_session(user_id, 'model', model3)
+    await update_user_session(user_id, 'version', 3)
+
+    await update.message.reply_text("Final Model Selected!")
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Help")
+    await update.message.reply_text("Helping...")
 
 
-async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Custom")
+async def end_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("The End...")
 
 
 # Responses
@@ -62,17 +133,18 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+    print(user_id, datetime.datetime.now())
+
     photo_file = await update.message.photo[-1].get_file()  # Get the file object
     photo_bytes = await photo_file.download_as_bytearray()  # Download as byte array
 
     photo_stream = BytesIO(photo_bytes)  # Create BytesIO object
-
     # Open the image using PIL and convert to RGB
     img = Image.open(photo_stream).convert('RGB')  # Ensure it's in RGB format
-
     img_array = preprocess_image(img)  # Preprocess the image
 
-    breed_name = predict_breed(model, img_array)
+    breed_name = predict_breed(user_sessions[user_id]['model'], img_array, user_sessions[user_id]['version'])
 
     await update.message.reply_text(f"The predicted breed is: {breed_name}")
 
@@ -86,8 +158,12 @@ if __name__ == '__main__':
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('restart', restart_command))
+    app.add_handler(CommandHandler('first', first_command))
+    app.add_handler(CommandHandler('second', second_command))
+    app.add_handler(CommandHandler('final', final_command))
     app.add_handler(CommandHandler('help', help_command))
-    app.add_handler(CommandHandler('custom_command', custom_command))
+    app.add_handler(CommandHandler('end', end_command))
 
     # Messages
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
